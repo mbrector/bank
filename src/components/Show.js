130774a39dafa3b969779b10e6c0d1b9839f27e3
unknown <mbrector@gmail.com>
2022-05-01 22:01:08 -0400

    total

--- src/components/Show.js
+++ src/components/Show.js
@@ -9,12 +9,14 @@ function Show() {
     const {id} = useParams()
     const [customer, setCustomer] = useState([])
     const [data, setData] = useState([])
+    let total = 0
+    
 
     useEffect(() => {
         getClient(id)
         .then(res => setCustomer(res.data))
         getAccounts()
-        .then(res => setData(res.data))
+        .then(res => setData(res.data))  
     }, [])
 
     data.sort((a, b)=>{
@@ -33,31 +35,29 @@ function Show() {
             deleteClient(id)
             navigate('/')
         }
-  return (
-    <div>
+        return (
+            <div>
         <h2>{customer.firstName} {customer.lastName}</h2><br />
-
 
         {data.map((account, i) => {
             return(<div key = {i}>
                         {customer._id==data[i].clientID?
                         <div>
+                            {console.log(total)}
                             {account.accountType}<br />               
                             ${account.amount}<br />
                             xxxxxx{account.accountNumber}<br /><br />
+                            <p hidden>{total += account.amount}</p>
                         </div>
                         :null}
                     </div>
-
                     )
                 })}
-        
-
-
         <button onClick={() => {navigate(`/${id}/transfer`)}}>Transfer</button>
         <button onClick={() => {navigate(`/${id}/newaccount`)}}>Add Account</button>
         <button onClick={() => {navigate(`/${id}/Edit`)}}>Edit Client</button>
         <button onClick={deleteTheClient}>Remove Client</button> 
+        <h2>${total}</h2>
     </div>
   )
 }
